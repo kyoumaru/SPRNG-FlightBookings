a3e920c6f717b41c8dcad0ba192dff9aa9765288
hpaguio <hendrickpaguio@gmail.com>
2025-11-27 20:18:31 +0800

    Booking view implemented

--- flightbookings/booking/urls.py
+++ flightbookings/booking/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("home/", views.home, name="home"),
-    path("booki/<int:flight_id>/", views.book_flight, name="book_flight"),
+    path("booking/<int:flight_id>/", views.book_flight, name="book_flight"),
     path("passenger/<int:passenger_id>/", views.passenger_bookings, name="passenger_bookings"),
 ]
